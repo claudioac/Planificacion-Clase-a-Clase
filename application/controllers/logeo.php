2510103b9e522c9f77242df1a1defeeb36f5ae59
Sebastián Salazar Molina <ssalazar@orangepeople.cl>
2014-01-03 17:18:42 -0300

    Arreglo en problema autenticación, creo  función helper

--- application/controllers/logeo.php
+++ application/controllers/logeo.php
@@ -1,4 +1,7 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Logeo extends CI_Controller {
 
@@ -6,104 +9,50 @@ class Logeo extends CI_Controller {
     public function __construct() {
         parent::__construct();
 //        $this->session_id =  $this->session->userdata('login');
+        $this->load->helper("ws_helper");
         session_start();
-        
     }
 
-    
-    public function index()
-	{
-                if(!empty($this->session_id))
-                {
-                    $this->load->view('Header');
-                    $this->load->view('index/inicio');
-                    $this->load->view('Footer');
-                }
-                else
-                {
-                    redirect(base_url('index.php/logeo/login'),301);   
-                }
-       
-		
-                
-	}
-        
-   public function login()
-   {
-       if($this->input->post())
-       {
-           $this->form_validation->set_rules('Usuarios', 'Usuarios','trim');
+    public function index() {
+        if (!empty($this->session_id)) {
+            $this->load->view('Header');
+            $this->load->view('index/inicio');
+            $this->load->view('Footer');
+        } else {
+            redirect(base_url('index.php/logeo/login'), 301);
+        }
+    }
+
+    public function login() {
+        if ($this->input->post()) {
+            $this->form_validation->set_rules('Usuarios', 'Usuarios', 'trim');
 //            die(sha1($this->input->post("Password",TRUE)));
-           if($this->form_validation->run())
-           {
+            if ($this->form_validation->run()) {
 //           $datos = $this->login_model->logeo($this->input->post("Usuario",TRUE), sha1($this->input->post("Password",TRUE)));
-           $rut = $this->input->post("Usuario",TRUE);
-           $p = strtoupper($this->input->post("password",TRUE));
-           $str = hash("SHA256", $p);
-           $resultado = autenticar($rut, $str);
+                $rut = $this->input->post("Usuario", TRUE);
+                $p = strtoupper($this->input->post("Password", TRUE));
+                $str = hash('sha256', $p);
+                error_log("$rut / $p / $str");
+                $resultado = ws_autenticar($rut, $str);
 //           echo $resultado;exit;
-           
-                if ($resultado) 
-                {
+
+                if ($resultado) {
                     $_SESSION['profesor'] = $this->input->post("Usuario", true);
                     redirect(base_url() . 'index.php/inicio', 301);
-                    
-                }
-                else {
-                    $this->session->set_flashdata('ControllerMessage','Usuario y/o clave invalida.');
-                    redirect(base_url('index.php/logeo/login'),301);  
-                }
-           
-           }
-           
-       }
-       $this->load->view('index/Login');
-       
-   }
-   
-        public function autenticar($rut , $contrasena)
-        {
-             $resultado = false;
-
-        try {
-
-        // Creacion de un arreglo con los parámetros de entrada.
-        $parametros = array();
-        $parametros['rut'] = $rut;
-        $parametros['password'] = $contrasena;
-
-        // usuario de webService
-        $autenticacion = array('login' => 'cacuna',
-            'password' => '30a7a0479c66576762bdc671041ceb1817ded11f');
-
-        $cliente = new SoapClient("http://informatica.utem.cl:8011/dirdoc-auth/ws/auth?wsdl", $autenticacion);
-        $objeto = $cliente->autenticar($parametros);
-        $codigo = (int) $objeto->return->codigo;
-        $descripcion = (string) verificar($objeto->return->descripcion);
-
-                if ($codigo > 0) {
-                    $resultado = true;
                 } else {
-                    error_log("Servicio WEB respondió: $descripcion ($codigo)");
+                    $this->session->set_flashdata('ControllerMessage', 'Usuario y/o clave invalida.');
+                    redirect(base_url('index.php/logeo/login'), 301);
                 }
-                } catch (Exception $e) {
-                $resultado = false;
-                error_log("Error en autenticacion: {$e->getMessage()}");
-                }
-            return $resultado;
+            }
         }
+        $this->load->view('index/Login');
+    }
 
-
-        public function logout()
-        {
+    public function logout() {
 //            $this->session->unset_userdata(array('login' => ''));
 //            $this->session->sess_destroy("planificacion");
         session_destroy();
-             redirect(base_url('index.php/logeo/login'),301);
-        }
-        
-        
+        redirect(base_url('index.php/logeo/login'), 301);
     }
-        
-        
-        
+
+}
---
+++ application/helpers/ws_helper.php
@@ -0,0 +1,97 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * 
+ * @file ws.php
+ * @author Sebastián Salazar Molina <sebasalazar@gmail.com>
+ */
+/**
+ * 
+ * @author Sebastián Salazar Molina <sebasalazar@gmail.com>
+ * @param string $rut
+ * @param string $contrasena
+ * @return boolean
+ * Valida contra el webservices de la unidad de informática, si el rut (con dígito verificador) 
+ * y la contraseña proporcionada (en mayúsculas y hasheada en sha256) es válida o no lo es.
+ */
+if (!function_exists('ws_autenticar')) {
+
+    function ws_autenticar($rut, $contrasena) {
+        $resultado = false;
+
+        try {
+
+            // Creacion de un arreglo con los parámetros de entrada.
+            $parametros = array();
+            $parametros['rut'] = $rut;
+            $parametros['password'] = $contrasena;
+
+            // usuario de webService
+            $autenticacion = array('login' => 'cacuna',
+                'password' => '30a7a0479c66576762bdc671041ceb1817ded11f');
+
+            $cliente = new SoapClient("http://informatica.utem.cl:8011/dirdoc-auth/ws/auth?wsdl", $autenticacion);
+            $objeto = $cliente->autenticar($parametros);
+            $codigo = (int) $objeto->return->codigo;
+            $descripcion = (string) trim($objeto->return->descripcion);
+
+            if ($codigo > 0) {
+                $resultado = true;
+            } else {
+                error_log("Servicio WEB respondió: $descripcion ($codigo)");
+            }
+        } catch (Exception $e) {
+            $resultado = false;
+            error_log("Error en autenticacion: {$e->getMessage()}");
+        }
+        return $resultado;
+    }
+
+}
+
+/**
+ * 
+ * @author Sebastián Salazar Molina <sebasalazar@gmail.com>
+ * @param string $rut
+ * @param int $semestre
+ * @param int $anio
+ * @return array
+ * Retorna los cursos de un docente segun semestre y año
+ */
+if (!function_exists('ws_cursos_semestre_anio')) {
+
+    function ws_cursos_semestre_anio($rut, $semestre, $anio) {
+        $resultado = array();
+
+        try {
+            // Creacion de un arreglo       
+            $parametros = array();
+            $parametros['rut'] = $rut;
+            $parametros['semestre'] = $semestre;
+            $parametros['anio'] = $anio;
+
+            // usuario de webService
+            $autenticacion = array('login' => 'cacuna',
+                'password' => '30a7a0479c66576762bdc671041ceb1817ded11f');
+
+            $cliente = new SoapClient("http://informatica.utem.cl:8011/dirdoc-auth/ws/auth?wsdl", $autenticacion);
+            $objeto = $cliente->consultarCursosPorRutDocenteYSemestre($parametros);
+            $respuesta = $objeto->return;
+            if (count($respuesta) > 1) {
+                foreach ($respuesta as $obj) {
+                    $resultado[] = (object) $obj;
+                }
+            } else {
+                $resultado[] = $respuesta;
+            }
+        } catch (Exception $e) {
+            $resultado = array();
+            error_log("Error en autenticacion: {$e->getMessage()}");
+        }
+        return $resultado;
+    }
+
+}
